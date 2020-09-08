fe108a60254703a7fd4ff229379bbad2a9f8d528
Niels van Ee <nielsvanee@hotmail.com>
2020-09-08 18:10:27 +0200

    physics epic

--- sketch.js
+++ sketch.js
@@ -1,5 +1,4 @@
-let e = 0.93
-let mu  = 0.005
+let mu  = 0.05
 let mBall = 0.17
 
 var resolution = [1920, 1080];
@@ -15,6 +14,8 @@ let ballGreen;
 let ballDarkRed;
 let ballBlack;
 let ballWhite;
+
+let frameTime;
 
 let ballDiameter =  0.025656;
 let ballRadius = ballDiameter / 2;
@@ -54,7 +55,8 @@ function setup()
     resolution = [windowWidth, windowHeight];
     createCanvas(resolution[0], resolution[1]);
     print(balls[-1])
-    balls[balls.length-1].vx = 0.4;
+    walls = [0, 0, 1, 0.5];
+    balls[balls.length-1].vx = 1;
     balls[balls.length-1].isMoving = true;
 }
   
@@ -77,18 +79,42 @@ function draw()
 
     fill(44, 130, 87);
     rect(resolution[0]/2 - 0.4*resolution[0], resolution[1]/2 - 0.2*resolution[0], 0.8*resolution[0], 0.4*resolution[0]);
-    
+
     //Ball rendering
     for (b of balls)
     {   
         b.x += b.vx * frameTime;
         b.y += b.vy * frameTime;
-        if (b.isMoving){
+        if (b.isMoving)
+        {
             b.isMoving = false;
+
+            let nextVelocityStep = b.nextVelocityStep();
+
+            //Collision Walls
+            if (b.hitXWall() !== 0){
+                b.vy  = Math.abs(b.vy)*b.hitXWall();
+                console.log('hit wall')
+            }
+            if (b.hitYWall() !== 0){
+                b.vx  = Math.abs(b.vx)*b.hitYWall();
+                console.log('hit wall')
+            }
+            //Collision Balls
+            for (b2 of balls)
+            {
+                if (b != b2 && !b.collidedWith.includes(b2) && b.dstToBall(b2) < 0 + 0*nextVelocityStep && false)
+                {
+                    b2.collidedWith.push(b);
+                    b.collidedWith.push(b2);
+                    ballCollision(b, b2);
+                }
+            }
+
+            //Moving
             if(b.vx !== 0){
                 b.isMoving = true;
-                b.vx += 9.81*mu*frameTime * -(b.vx/Math.abs(b.vx));
-                console.log(b.vx)
+                b.vx += 9.81/2*mu*frameTime * -(b.vx/Math.abs(b.vx));
                 if (Math.abs(b.vx) <= 0.001){
                     b.vx = 0;
                 }
@@ -104,14 +130,18 @@ function draw()
         fill(b.color);
         circle(convertX(b.x), convertY(b.y), ballDiameter*0.8*resolution[0]);
     }
-
+    for(b of balls)
+    {
+        b.collidedWith = [];
+    }
 }
+
+
 
 function windowResized() 
 {
     resizeCanvas(windowWidth, windowHeight);
     resolution = [windowWidth, windowHeight];
-    walls = [resolution[0]/2 - 0.4*resolution[0], resolution[1]/2 - 0.2*resolution[0], resolution[0]/2 + 0.4*resolution[0], resolution[1]/2 + 0.2*resolution[0]];
 }
 
 class Ball
@@ -128,54 +158,73 @@ class Ball
         this.isEight = false;
         this.color = _color;
         this.number = _number;
+        this.collidedWith = [];
     }
 
     dstToBall(b)
     {
-        return Math.sqrt(Math.pow(x-b.x,2) + Math.pow(y-b.y,2)) - ballDiameter;
+        return Math.sqrt(Math.pow(this.x-b.x,2) + Math.pow(this.y-b.y,2)) - ballDiameter;
     }
     
-    dstToYWall()
+    hitXWall()
     {
-        dstNorth = this.y - walls[1];
-        dstSouth = walls[3] - this.y;
-        return Math.min(dstNorth, dstSouth);
+        if (this.y <= ballRadius || this.y >= 0.5 - ballRadius)
+            return true;
+        return false;
     }
 
-    dstToXWall()
+    hitYWall()
     {
-        dstWest = this.x - walls[0];
-        dstEast = walls[2] - this.x;
-        return Math.min(dstEast, dstWest);
+        if ((this.x <= ballRadius))
+            return 1;
+        if  (this.x >= 1 - ballRadius){
+            return -1;
+        }
+        return 0;
+    }
+
+    nextVelocityStep()
+    {
+        return Math.sqrt(Math.pow(this.vx, 2) + Math.pow(this.vy, 2));
     }
 }
 
 
-function convertX(x){
-
-    return resolution[0]/2 -  0.4*resolution[0] + x*0.8*resolution[0]
+function convertX(x)
+{
+    return resolution[0]/2 - 0.4*resolution[0] + x*0.8*resolution[0]
 }
 
 
 function convertY(y){
-    return resolution[1]/2 -  0.2*resolution[0] + y*0.8*resolution[0]
+    return resolution[1]/2 - 0.2*resolution[0] + y*0.8*resolution[0]
 }
 
 function ballCollision(b1, b2){
-    theta1 = Math.atan(b1.vy/b1.vx)
-    theta2 = Math.atan(b1.vy/b1.vx)
-    phi = Math.atan((b1.y - b2.y) / (b1.x - b2.x))
+    let theta1 = Math.atan(b1.vy/b1.vx)
+    let theta2 = Math.atan(b1.vy/b1.vx)
+    let phi = Math.atan((b1.y - b2.y) / (b1.x - b2.x))
 
     vx1 = b2.vx*Math.cos(theta2-phi)*Math.cos(phi) + b1.vx*Math.sin(theta1 - phi)*Math.cos(phi+Math.PI/2)
     vy1 = b2.vy*Math.cos(theta2-phi)*Math.sin(phi) + b1.vy*Math.sin(theta1 - phi)*Math.sin(phi+Math.PI/2)
 
     vx2 = b1.vx*Math.cos(theta1-phi)*Math.cos(phi) + b2.vx*Math.sin(theta2 - phi)*Math.cos(phi+Math.PI/2)
     vy2 = b1.vy*Math.cos(theta1-phi)*Math.sin(phi) + b2.vy*Math.sin(theta2 - phi)*Math.sin(phi+Math.PI/2)
+    
+    //set balls back on their previous path a bit to get them to separate nicely
+
+    
 
     b1.vx = vx1
     b1.vy = vy1
     b2.vx = vx2
     b2.vy = vy2
+
+    //set balls back on their previous path a bit to get them to separate nicely
+
+    b1.isMoving = true;
+    b2.isMoving = true;
+
 
 }
 //team true --> striped
