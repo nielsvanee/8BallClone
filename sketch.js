7ac3edd73f81ba164b6fd1c92c34bc2e2a061c26
Robin ter Heide <robin@terheide.eu>
2020-09-08 02:19:57 +0200

    nice meme 2

--- sketch.js
+++ sketch.js
@@ -1,5 +1,8 @@
 //niels is a nagger
 
+let e = 0.93
+let mu  = 0.005
+
 var resolution = [1920, 1080];
 var walls = [];
 var balls = [];
@@ -14,8 +17,16 @@ let ballDarkRed;
 let ballBlack;
 let ballWhite;
 
-let ballDiameter = 0.02*resolution[0];
+let ballDiameter =  0.025656;
 let ballRadius = ballDiameter / 2;
+
+let xHeightDifference = Math.cos(Math.PI/6)*ballDiameter
+let yHeightDifference = Math.sin(Math.PI/6)*ballDiameter
+
+let startCoordinates = [[0.75, 0.25], [0.75 + 3*xHeightDifference, 0.25 + 3*yHeightDifference], [0.75 + 2*xHeightDifference, 0.25 + 2*yHeightDifference], [0.75 + 3*xHeightDifference, 0.25 - yHeightDifference], 
+                        [0.75 + 2*xHeightDifference, 0.25 - 2*yHeightDifference],  [0.75 + 4*xHeightDifference, 0.25 + 4*yHeightDifference], [0.75 + 4*xHeightDifference, 0.25], 
+                        [0.75 + 4*xHeightDifference, 0.25 - 2*yHeightDifference], [0.75 + 4*xHeightDifference, 0.25 + 2*yHeightDifference], [0.75 + 4*xHeightDifference, 0.25 - 4*yHeightDifference], [0.75 + 3*xHeightDifference, 0.25 - 3*yHeightDifference],
+                        [0.75 + xHeightDifference, 0.25 + yHeightDifference], [0.75 + 3*xHeightDifference, 0.25 + yHeightDifference], [0.75 + xHeightDifference, 0.25 - yHeightDifference]]
 
 let Ycenterline;
 
@@ -31,32 +42,29 @@ function setup()
     ballDarkRed = color(131, 20, 29);
     ballBlack = color(25, 23, 24);
     ballWhite = color(216, 207, 178);
-    
     Ycenterline = resolution[1]/2
-
     colors = [ballYellow, ballBlue, ballRed, ballPurple, ballOrange, ballGreen, ballDarkRed];
-    for (i = 0; i <= colors.length - 2; i++){
-        balls.push(new Ball(false, 100, 100, colors[i], i+1));
-        balls.push(new Ball(true, 100, 100, colors[i], i+9));
+    for (i = 0; i <= colors.length - 1 ; i++){
+        balls.push(new Ball(false, startCoordinates[i][0], startCoordinates[i][1], colors[i], i+1));
+        balls.push(new Ball(true, startCoordinates[i+7][0], startCoordinates[i+7][1], colors[i], i+9));
     }
-    balls.push(new Ball("black", 0, 0, ballBlack, 8));
-    balls.push(new Ball("white", 0.4*resolution[0], Ycenterline, ballWhite, 0));
+    balls.push(new Ball("black", 0.75 + 2*xHeightDifference, 0.25, ballBlack, 8));
+    balls.push(new Ball("white", 0.25, 0.25, ballWhite, 0));
     resolution = [windowWidth, windowHeight];
     createCanvas(resolution[0], resolution[1]);
 }
   
 function draw() 
 {
-    
     background(255);
     fill(44, 130, 87);
     rect(resolution[0]/2 -  0.4*resolution[0], resolution[1]/2 -  0.2*resolution[0], 0.8*resolution[0], 0.4*resolution[0]);
     
     //Ball rendering
     for (b of balls)
-    {
+    { 
         fill(b.color);
-        circle(b.x, b.y, ballDiameter);
+        circle(convertX(b.x), convertY(b.y), ballDiameter*0.8*resolution[0]);
     }
 
 }
@@ -105,4 +113,15 @@ class Ball
 }
 
 
+function convertX(x){
+
+    return resolution[0]/2 -  0.4*resolution[0] + x*0.8*resolution[0]
+}
+
+
+function convertY(y){
+    return resolution[1]/2 -  0.2*resolution[0] + y*0.8*resolution[0]
+}
+
 //team true --> striped
+//lokaal coordinaten systeem origin in top left corner van tafel, lengte = 1 uts, hoogte = 0.5 uts
